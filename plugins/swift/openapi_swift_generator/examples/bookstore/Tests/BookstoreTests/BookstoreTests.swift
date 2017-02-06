b7d4a76a955c48a6fd840b2a5caf1462db4211f3
Tim Burks <timburks@google.com>
2017-02-05 20:08:03 -0800

    Fix compiler warnings in Swift BookstoreTests.swift (openapi_swift_generator)

--- plugins/swift/openapi_swift_generator/examples/bookstore/Tests/BookstoreTests/BookstoreTests.swift
+++ plugins/swift/openapi_swift_generator/examples/bookstore/Tests/BookstoreTests/BookstoreTests.swift
@@ -9,157 +9,153 @@ func Log(_ message : String) {
 let service = "http://localhost:8090"
 
 class BookstoreTests: XCTestCase {
-  
+
   func testBasic() {
     // create a client
-    let b = Bookstore.Client(service:service)    
+    let b = Bookstore.Client(service:service)
     Log("// reset the service by deleting all shelves")
     do {
-        try b.deleteShelves()       
+      try b.deleteShelves()
     } catch (let error) {
-        XCTFail("\(error)")                 
-    }    
+      XCTFail("\(error)")
+    }
     Log("// verify that the service has no shelves")
     do {
-        let response = try b.listShelves()  
-        XCTAssertEqual(response.shelves.count, 0)
+      let response = try b.listShelves()
+      XCTAssertEqual(response.shelves.count, 0)
     } catch (let error) {
-        XCTFail("\(error)")                 
+      XCTFail("\(error)")
     }
     Log("// attempting to get a shelf should return an error")
     do {
-        let _ = try b.getShelf(shelf:1)
-        XCTFail("server error")             
-    } catch (let error) {
+      let _ = try b.getShelf(shelf:1)
+      XCTFail("server error")
+    } catch {
     }
     Log("// attempting to get a book should return an error")
-    do { 
-        let _ = try b.getBook(shelf:1, book:2)
-    } catch (let error) {
-    }       
+    do {
+      let _ = try b.getBook(shelf:1, book:2)
+    } catch {
+    }
     Log("// add a shelf")
     do {
-        var shelf = Shelf()
-        shelf.theme = "mysteries"
-        let response = try b.createShelf(shelf:shelf)
-        if (response.name != "shelves/1") ||
-            (response.theme != "mysteries") {
-             XCTFail("mismatch")                    
-        }
+      let shelf = Shelf()
+      shelf.theme = "mysteries"
+      let response = try b.createShelf(shelf:shelf)
+      if (response.name != "shelves/1") ||
+        (response.theme != "mysteries") {
+        XCTFail("mismatch")
+      }
     } catch (let error) {
-        XCTFail("\(error)")                 
+      XCTFail("\(error)")
     }
-   Log("// add another shelf")
-   do {
-       var shelf = Shelf()
-       shelf.theme = "comedies"
-       let response = try b.createShelf(shelf:shelf)
-       if (response.name != "shelves/2") ||
-           (response.theme != "comedies") {
-            XCTFail("mismatch")                    
-       }
-   } catch (let error) {
-       XCTFail("\(error)")                 
-   }
+    Log("// add another shelf")
+    do {
+      let shelf = Shelf()
+      shelf.theme = "comedies"
+      let response = try b.createShelf(shelf:shelf)
+      if (response.name != "shelves/2") ||
+        (response.theme != "comedies") {
+        XCTFail("mismatch")
+      }
+    } catch (let error) {
+      XCTFail("\(error)")
+    }
     Log("// get the first shelf that was added")
     do {
-        let response = try b.getShelf(shelf:1)
-        if (response.name != "shelves/1") ||
-            (response.theme != "mysteries") {
-             XCTFail("mismatch")                    
-        }
+      let response = try b.getShelf(shelf:1)
+      if (response.name != "shelves/1") ||
+        (response.theme != "mysteries") {
+        XCTFail("mismatch")
+      }
     } catch (let error) {
-        XCTFail("\(error)")                 
+      XCTFail("\(error)")
     }
     Log("// list shelves and verify that there are 2")
     do {
-        let response = try b.listShelves()  
-        XCTAssertEqual(response.shelves.count, 2)
+      let response = try b.listShelves()
+      XCTAssertEqual(response.shelves.count, 2)
     } catch (let error) {
-        XCTFail("\(error)")                 
-    }	
+      XCTFail("\(error)")
+    }
     Log("// delete a shelf")
     do {
-        try b.deleteShelf(shelf:2)       
+      try b.deleteShelf(shelf:2)
     } catch (let error) {
-        XCTFail("\(error)")                 
+      XCTFail("\(error)")
     }
     Log("// list shelves and verify that there is only 1")
     do {
-        let response = try b.listShelves()  
-        XCTAssertEqual(response.shelves.count, 1)
+      let response = try b.listShelves()
+      XCTAssertEqual(response.shelves.count, 1)
     } catch (let error) {
-        XCTFail("\(error)")                 
+      XCTFail("\(error)")
     }
     Log("// list books on a shelf, verify that there are none")
     do {
-        let response = try b.listBooks(shelf:1)  
-        XCTAssertEqual(response.books.count, 0)
+      let response = try b.listBooks(shelf:1)
+      XCTAssertEqual(response.books.count, 0)
     } catch (let error) {
-        XCTFail("\(error)")                 
+      XCTFail("\(error)")
     }
     Log("// create a book")
     do {
-        var book = Book()
-        book.author = "Agatha Christie"
-        book.title = "And Then There Were None"
-        let _ = try b.createBook(shelf:1, book:book)
+      let book = Book()
+      book.author = "Agatha Christie"
+      book.title = "And Then There Were None"
+      let _ = try b.createBook(shelf:1, book:book)
     } catch (let error) {
-        XCTFail("\(error)")                 
-    }	
+      XCTFail("\(error)")
+    }
     Log("// create another book")
     do {
-        var book = Book()
-        book.author = "Agatha Christie"
-        book.title = "Murder on the Orient Express"
-        let _ = try b.createBook(shelf:1, book:book)
+      let book = Book()
+      book.author = "Agatha Christie"
+      book.title = "Murder on the Orient Express"
+      let _ = try b.createBook(shelf:1, book:book)
     } catch (let error) {
-        XCTFail("\(error)")                 
+      XCTFail("\(error)")
     }
     Log("// get the first book that was added")
     do {
-        let response = try b.getBook(shelf:1, book:1)
-        if (response.author != "Agatha Christie") ||
-            (response.title != "And Then There Were None") {
-             XCTFail("mismatch")                    
-        }
+      let response = try b.getBook(shelf:1, book:1)
+      if (response.author != "Agatha Christie") ||
+        (response.title != "And Then There Were None") {
+        XCTFail("mismatch")
+      }
     } catch (let error) {
-        XCTFail("\(error)")                 
+      XCTFail("\(error)")
     }
     Log("// list the books on a shelf and verify that there are 2")
     do {
-        let response = try b.listBooks(shelf:1)  
-        XCTAssertEqual(response.books.count, 2)
+      let response = try b.listBooks(shelf:1)
+      XCTAssertEqual(response.books.count, 2)
     } catch (let error) {
-        XCTFail("\(error)")                 
+      XCTFail("\(error)")
     }
     Log("// delete a book")
     do {
-        try b.deleteBook(shelf:1, book:2)       
+      try b.deleteBook(shelf:1, book:2)
     } catch (let error) {
-        XCTFail("\(error)")                 
+      XCTFail("\(error)")
     }
     Log("// list the books on a shelf and verify that is only 1")
     do {
-        let response = try b.listBooks(shelf:1)  
-        XCTAssertEqual(response.books.count, 1)
+      let response = try b.listBooks(shelf:1)
+      XCTAssertEqual(response.books.count, 1)
     } catch (let error) {
-        XCTFail("\(error)")                 
+      XCTFail("\(error)")
     }
     Log("// verify the handling of a badly-formed request")
-    do {
-  	  var path = service 
-  	  path = path + "/shelves"
-  	  let url = URL(string:path)
-  	  var request = URLRequest(url:url!)
-  	  request.httpMethod = "POST"
-  	  request.httpBody = "".data(using:.utf8)
-  	  let (data, response, error) = URLSession.shared.fetch(request)	  
-      // we expect a 400 (Bad Request) code  	  
-      XCTAssertEqual((response as! HTTPURLResponse).statusCode, 400)
-    } catch (let error) {
-        XCTFail("\(error)")                 
-    }
+    var path = service
+    path = path + "/shelves"
+    let url = URL(string:path)
+    var request = URLRequest(url:url!)
+    request.httpMethod = "POST"
+    request.httpBody = "".data(using:.utf8)
+    let (_, response, _) = URLSession.shared.fetch(request)
+    // we expect a 400 (Bad Request) code
+    XCTAssertEqual((response as! HTTPURLResponse).statusCode, 400)
   }
 }
 
