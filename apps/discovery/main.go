db1e15f2831398fa70755994a5fc6e1c508b8e06
Tim Burks <timburks@google.com>
2017-06-23 12:40:16 -0700

    First pseudo-complete conversion from Discovery Format to OpenAPIv2.

--- apps/discovery/main.go
+++ apps/discovery/main.go
@@ -1,3 +1,16 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package main
 
 import (
@@ -21,7 +34,7 @@ func main() {
 	apiListServiceURL := "https://www.googleapis.com/discovery/v1/apis"
 	bytes, err := compiler.FetchFile(apiListServiceURL)
 	if err != nil {
-		log.Fatal("%+v", err)
+		log.Fatalf("%+v", err)
 	}
 	// Unpack the apis/list response.
 	listResponse, err := disco.NewList(bytes)
@@ -32,22 +45,22 @@ func main() {
 	// Get the description of an API
 	api := listResponse.APIWithID(apiName + ":" + apiVersion)
 	if api == nil {
-		log.Fatal("Error: API not found")
+		log.Fatalf("Error: API not found")
 	}
-	fmt.Printf("API: %+v\n", api)
+	//fmt.Printf("API: %+v\n", api)
 	// Fetch the discovery description of the API.
 	bytes, err = compiler.FetchFile(api.DiscoveryRestURL)
 	if err != nil {
-		log.Fatal("%+v", err)
+		log.Fatalf("%+v", err)
 	}
 	// Unpack the discovery response.
 	discoveryDocument, err := disco.NewDocument(bytes)
 	if err != nil {
-		log.Fatal("%+v", err)
+		log.Fatalf("%+v", err)
 	}
-	fmt.Printf("DISCOVERY: %+v\n", discoveryDocument)
+	//fmt.Printf("DISCOVERY: %+v\n", discoveryDocument)
 	// Generate the OpenAPI equivalent
-	openAPIDocument := buildOpenAPI2Document(discoveryDocument)
+	openAPIDocument := buildOpenAPI2DocumentForDocument(discoveryDocument)
 	bytes, err = proto.Marshal(openAPIDocument)
 	if err != nil {
 		panic(err)
@@ -59,7 +72,7 @@ func main() {
 }
 
 func addOpenAPI2SchemaForSchema(d *pb.Document, name string, schema *disco.Schema) {
-	fmt.Printf("SCHEMA %s\n", name)
+	//log.Printf("SCHEMA %s\n", name)
 	d.Definitions.AdditionalProperties = append(d.Definitions.AdditionalProperties,
 		&pb.NamedSchema{
 			Name:  schema.Name,
@@ -103,75 +116,97 @@ func buildOpenAPI2SchemaForSchema(schema *disco.Schema) *pb.Schema {
 	return s
 }
 
-func buildOpenAPI2OperationForMethod(method *disco.Method) *pb.Operation {
-	fmt.Printf("METHOD %s %s %s %s\n", method.Name, method.FlatPath, method.HTTPMethod, method.ID)
-	//fmt.Printf("MAP %+v\n", method.JSONMap)
-
-	parameters := make([]*pb.ParametersItem, 0)
-	for _, p := range method.Parameters {
-		fmt.Printf("- PARAMETER %+v\n", p)
-		parameters = append(parameters,
-			&pb.ParametersItem{
-				Oneof: &pb.ParametersItem_Parameter{
-					Parameter: &pb.Parameter{
-						Oneof: &pb.Parameter_NonBodyParameter{
-							NonBodyParameter: &pb.NonBodyParameter{
-								Oneof: &pb.NonBodyParameter_QueryParameterSubSchema{
-									QueryParameterSubSchema: &pb.QueryParameterSubSchema{
-										Name:        p.Name,
-										In:          "XXX",
-										Description: p.Description,
-										Required:    false,
-										Type:        "XXX",
-										Format:      "XXX",
-									},
-								},
-							},
-						},
-					},
-				},
-			})
-	}
-
-	fmt.Printf("- RESPONSE %+v\n", method.Response)
-
-	responses := &pb.Responses{
-		ResponseCode: []*pb.NamedResponseValue{
-			&pb.NamedResponseValue{
-				Name: "201",
-				Value: &pb.ResponseValue{
-					Oneof: &pb.ResponseValue_Response{
-						Response: &pb.Response{
-							Description: "Null response",
+func buildOpenAPI2ParameterForParameter(p *disco.Parameter) *pb.Parameter {
+	//log.Printf("- PARAMETER %+v\n", p.Name)
+	typeName := p.Schema.Type
+	format := p.Schema.Format
+	location := p.Location
+	switch location {
+	case "query":
+		return &pb.Parameter{
+			Oneof: &pb.Parameter_NonBodyParameter{
+				NonBodyParameter: &pb.NonBodyParameter{
+					Oneof: &pb.NonBodyParameter_QueryParameterSubSchema{
+						QueryParameterSubSchema: &pb.QueryParameterSubSchema{
+							Name:        p.Name,
+							In:          "query",
+							Description: p.Description,
+							Required:    false,
+							Type:        typeName,
+							Format:      format,
 						},
 					},
 				},
 			},
+		}
+	case "path":
+		return &pb.Parameter{
+			Oneof: &pb.Parameter_NonBodyParameter{
+				NonBodyParameter: &pb.NonBodyParameter{
+					Oneof: &pb.NonBodyParameter_PathParameterSubSchema{
+						PathParameterSubSchema: &pb.PathParameterSubSchema{
+							Name:        p.Name,
+							In:          "path",
+							Description: p.Description,
+							Required:    false,
+							Type:        typeName,
+							Format:      format,
+						},
+					},
+				},
+			},
+		}
+	default:
+		return nil
+	}
+}
+
+func buildOpenAPI2ResponseForSchema(schema *disco.Schema) *pb.Response {
+	//log.Printf("- RESPONSE %+v\n", schema)
+	ref := schema.Ref
+	if ref == "" {
+		log.Printf("ERROR: UNHANDLED RESPONSE SCHEMA %+v", schema)
+	}
+	return &pb.Response{
+		Description: "Successful operation",
+		Schema: &pb.SchemaItem{
+			Oneof: &pb.SchemaItem_Schema{
+				Schema: &pb.Schema{
+					XRef: "#/definitions/" + ref,
+				},
+			},
+		},
+	}
+}
+
+func buildOpenAPI2OperationForMethod(method *disco.Method) *pb.Operation {
+	//log.Printf("METHOD %s %s %s %s\n", method.Name, method.FlatPath, method.HTTPMethod, method.ID)
+	//log.Printf("MAP %+v\n", method.JSONMap)
+	parameters := make([]*pb.ParametersItem, 0)
+	for _, p := range method.Parameters {
+		parameters = append(parameters, &pb.ParametersItem{
+			Oneof: &pb.ParametersItem_Parameter{
+				Parameter: buildOpenAPI2ParameterForParameter(p),
+			},
+		})
+	}
+	responses := &pb.Responses{
+		ResponseCode: []*pb.NamedResponseValue{
 			&pb.NamedResponseValue{
 				Name: "default",
 				Value: &pb.ResponseValue{
 					Oneof: &pb.ResponseValue_Response{
-						Response: &pb.Response{
-							Description: "unexpected error",
-							Schema: &pb.SchemaItem{
-								Oneof: &pb.SchemaItem_Schema{
-									Schema: &pb.Schema{
-										XRef: "#/definitions/Error",
-									},
-								},
-							},
-						},
+						Response: buildOpenAPI2ResponseForSchema(method.Response),
 					},
 				},
 			},
 		},
 	}
-
 	return &pb.Operation{
 		Summary:     method.Description,
 		OperationId: method.ID,
-		Responses:   responses,
 		Parameters:  parameters,
+		Responses:   responses,
 	}
 }
 
@@ -206,12 +241,12 @@ func addOpenAPI2PathsForMethod(d *pb.Document, method *disco.Method) {
 	case "DELETE":
 		pathItem.Delete = operation
 	default:
-		log.Printf("UNKNOWN HTTP METHOD %s", method.HTTPMethod)
+		log.Printf("ERROR: UNKNOWN HTTP METHOD %s", method.HTTPMethod)
 	}
 }
 
 func addOpenAPI2PathsForResource(d *pb.Document, resource *disco.Resource) {
-	fmt.Printf("RESOURCE %s (%s)\n", resource.Name, resource.FullName)
+	//log.Printf("RESOURCE %s (%s)\n", resource.Name, resource.FullName)
 	for _, method := range resource.Methods {
 		addOpenAPI2PathsForMethod(d, method)
 	}
@@ -220,7 +255,7 @@ func addOpenAPI2PathsForResource(d *pb.Document, resource *disco.Resource) {
 	}
 }
 
-func buildOpenAPI2Document(api *disco.Document) *pb.Document {
+func buildOpenAPI2DocumentForDocument(api *disco.Document) *pb.Document {
 	d := &pb.Document{}
 	d.Swagger = "2.0"
 	d.Info = &pb.Info{
@@ -228,7 +263,6 @@ func buildOpenAPI2Document(api *disco.Document) *pb.Document {
 		Version:     api.Version,
 		Description: api.Description,
 	}
-
 	url, _ := url.Parse(api.RootURL)
 	d.Host = url.Host
 	d.BasePath = url.Path
@@ -237,245 +271,14 @@ func buildOpenAPI2Document(api *disco.Document) *pb.Document {
 	d.Produces = []string{"application/json"}
 	d.Paths = &pb.Paths{}
 	d.Definitions = &pb.Definitions{}
-
 	for name, schema := range api.Schemas {
 		addOpenAPI2SchemaForSchema(d, name, schema)
 	}
-
 	for _, method := range api.Methods {
 		addOpenAPI2PathsForMethod(d, method)
 	}
-
 	for _, resource := range api.Resources {
 		addOpenAPI2PathsForResource(d, resource)
 	}
-
-	/*
-		d.Paths.Path = append(d.Paths.Path,
-			&pb.NamedPathItem{
-				Name: "/pets",
-				Value: &pb.PathItem{
-					Get: &pb.Operation{
-						Summary:     "List all pets",
-						OperationId: "listPets",
-						Tags:        []string{"pets"},
-						Parameters: []*pb.ParametersItem{
-							&pb.ParametersItem{
-								Oneof: &pb.ParametersItem_Parameter{
-									Parameter: &pb.Parameter{
-										Oneof: &pb.Parameter_NonBodyParameter{
-											NonBodyParameter: &pb.NonBodyParameter{
-												Oneof: &pb.NonBodyParameter_QueryParameterSubSchema{
-													QueryParameterSubSchema: &pb.QueryParameterSubSchema{
-														Name:        "limit",
-														In:          "query",
-														Description: "How many items to return at one time (max 100)",
-														Required:    false,
-														Type:        "integer",
-														Format:      "int32",
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-						Responses: &pb.Responses{
-							ResponseCode: []*pb.NamedResponseValue{
-								&pb.NamedResponseValue{
-									Name: "200",
-									Value: &pb.ResponseValue{
-										Oneof: &pb.ResponseValue_Response{
-											Response: &pb.Response{
-												Description: "An paged array of pets", // [sic] match other examples
-												Schema: &pb.SchemaItem{
-													Oneof: &pb.SchemaItem_Schema{
-														Schema: &pb.Schema{
-															XRef: "#/definitions/Pets",
-														},
-													},
-												},
-												Headers: &pb.Headers{
-													AdditionalProperties: []*pb.NamedHeader{
-														&pb.NamedHeader{
-															Name: "x-next",
-															Value: &pb.Header{
-																Type:        "string",
-																Description: "A link to the next page of responses",
-															},
-														},
-													},
-												},
-											},
-										},
-									},
-								},
-								&pb.NamedResponseValue{
-									Name: "default",
-									Value: &pb.ResponseValue{
-										Oneof: &pb.ResponseValue_Response{
-											Response: &pb.Response{
-												Description: "unexpected error",
-												Schema: &pb.SchemaItem{
-													Oneof: &pb.SchemaItem_Schema{
-														Schema: &pb.Schema{
-															XRef: "#/definitions/Error",
-														},
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-					Post: &pb.Operation{
-						Summary:     "Create a pet",
-						OperationId: "createPets",
-						Tags:        []string{"pets"},
-						Parameters:  []*pb.ParametersItem{},
-						Responses: &pb.Responses{
-							ResponseCode: []*pb.NamedResponseValue{
-								&pb.NamedResponseValue{
-									Name: "201",
-									Value: &pb.ResponseValue{
-										Oneof: &pb.ResponseValue_Response{
-											Response: &pb.Response{
-												Description: "Null response",
-											},
-										},
-									},
-								},
-								&pb.NamedResponseValue{
-									Name: "default",
-									Value: &pb.ResponseValue{
-										Oneof: &pb.ResponseValue_Response{
-											Response: &pb.Response{
-												Description: "unexpected error",
-												Schema: &pb.SchemaItem{
-													Oneof: &pb.SchemaItem_Schema{
-														Schema: &pb.Schema{
-															XRef: "#/definitions/Error",
-														},
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				}})
-		d.Paths.Path = append(d.Paths.Path,
-			&pb.NamedPathItem{
-				Name: "/pets/{petId}",
-				Value: &pb.PathItem{
-					Get: &pb.Operation{
-						Summary:     "Info for a specific pet",
-						OperationId: "showPetById",
-						Tags:        []string{"pets"},
-						Parameters: []*pb.ParametersItem{
-							&pb.ParametersItem{
-								Oneof: &pb.ParametersItem_Parameter{
-									Parameter: &pb.Parameter{
-										Oneof: &pb.Parameter_NonBodyParameter{
-											NonBodyParameter: &pb.NonBodyParameter{
-												Oneof: &pb.NonBodyParameter_PathParameterSubSchema{
-													PathParameterSubSchema: &pb.PathParameterSubSchema{
-														Name:        "petId",
-														In:          "path",
-														Description: "The id of the pet to retrieve",
-														Required:    true,
-														Type:        "string",
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-						Responses: &pb.Responses{
-							ResponseCode: []*pb.NamedResponseValue{
-								&pb.NamedResponseValue{
-									Name: "200",
-									Value: &pb.ResponseValue{
-										Oneof: &pb.ResponseValue_Response{
-											Response: &pb.Response{
-												Description: "Expected response to a valid request",
-												Schema: &pb.SchemaItem{
-													Oneof: &pb.SchemaItem_Schema{
-														Schema: &pb.Schema{
-															XRef: "#/definitions/Pets",
-														},
-													},
-												},
-											},
-										},
-									},
-								},
-								&pb.NamedResponseValue{
-									Name: "default",
-									Value: &pb.ResponseValue{
-										Oneof: &pb.ResponseValue_Response{
-											Response: &pb.Response{
-												Description: "unexpected error",
-												Schema: &pb.SchemaItem{
-													Oneof: &pb.SchemaItem_Schema{
-														Schema: &pb.Schema{
-															XRef: "#/definitions/Error",
-														},
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				}})
-	*/
-	/*
-		d.Definitions.AdditionalProperties = append(d.Definitions.AdditionalProperties,
-			&pb.NamedSchema{
-				Name: "Pet",
-				Value: &pb.Schema{
-					Required: []string{"id", "name"},
-					Properties: &pb.Properties{
-						AdditionalProperties: []*pb.NamedSchema{
-							&pb.NamedSchema{Name: "id", Value: &pb.Schema{
-								Type:   &pb.TypeItem{[]string{"integer"}},
-								Format: "int64"}},
-							&pb.NamedSchema{Name: "name", Value: &pb.Schema{Type: &pb.TypeItem{[]string{"string"}}}},
-							&pb.NamedSchema{Name: "tag", Value: &pb.Schema{Type: &pb.TypeItem{[]string{"string"}}}},
-						},
-					},
-				}})
-		d.Definitions.AdditionalProperties = append(d.Definitions.AdditionalProperties,
-			&pb.NamedSchema{
-				Name: "Pets",
-				Value: &pb.Schema{
-					Type:  &pb.TypeItem{[]string{"array"}},
-					Items: &pb.ItemsItem{[]*pb.Schema{&pb.Schema{XRef: "#/definitions/Pet"}}},
-				}})
-		d.Definitions.AdditionalProperties = append(d.Definitions.AdditionalProperties,
-			&pb.NamedSchema{
-				Name: "Error",
-				Value: &pb.Schema{
-					Required: []string{"code", "message"},
-					Properties: &pb.Properties{
-						AdditionalProperties: []*pb.NamedSchema{
-							&pb.NamedSchema{Name: "code", Value: &pb.Schema{
-								Type:   &pb.TypeItem{[]string{"integer"}},
-								Format: "int32"}},
-							&pb.NamedSchema{Name: "message", Value: &pb.Schema{Type: &pb.TypeItem{[]string{"string"}}}},
-						},
-					},
-				}})
-	*/
 	return d
 }
