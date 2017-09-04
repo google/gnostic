e815c2236bfb6855751a013ce4e9ca05cad278a0
Tim Burks <timburks@google.com>
2017-09-03 20:52:51 -0700

    Rename surface package.

--- surface/type.go
+++ surface/type.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package gnostic_surface_v1
+package surface_v1
 
 func (t *Type) addField(f *Field) {
 	t.Fields = append(t.Fields, f)
