3fe8a17371881bcaf4df1cafe810c1d3fdc90b24
Tim Burks <timburks@google.com>
2017-09-03 20:47:16 -0700

    Move code generation surface to gnostic top-level.

--- plugins/gnostic-go-generator/render_types.go
+++ plugins/gnostic-go-generator/render_types.go
@@ -15,7 +15,7 @@
 package main
 
 import (
-	surface "github.com/googleapis/gnostic/plugins/gnostic-go-generator/surface"
+	surface "github.com/googleapis/gnostic/surface"
 )
 
 func (renderer *Renderer) RenderTypes() ([]byte, error) {
