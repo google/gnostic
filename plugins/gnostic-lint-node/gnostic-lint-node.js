0bf41be0762db710ffe4b53de0750c7128a9c056
Tim Burks <timburks@google.com>
2017-12-07 22:46:59 -0800

    Minor improvements to the node linter.

--- plugins/gnostic-lint-node/gnostic-lint-node.js
+++ plugins/gnostic-lint-node/gnostic-lint-node.js
@@ -1,21 +1,18 @@
 // import libraries
-const protobuf = require("protobufjs");
-const getStdin = require('get-stdin');
+const protobuf = require("protobufjs")
+const getStdin = require('get-stdin')
 
 // import messages
-var root = protobuf.Root.fromJSON(require("./bundle.json"));
-var Document = root.lookupType("openapi.v2.Document")
-var Request = root.lookupType("gnostic.plugin.v1.Request")
-var Response = root.lookupType("gnostic.plugin.v1.Response")
+const root = protobuf.Root.fromJSON(require("./bundle.json"))
+const Request = root.lookupType("gnostic.plugin.v1.Request")
+const Response = root.lookupType("gnostic.plugin.v1.Response")
 
 getStdin.buffer().then(buffer => {
-	var request = Request.decode(buffer);
+	const request = Request.decode(buffer)
 	console.error('message: %s\n\n', JSON.stringify(request))
 
-	var openapi2 = request.openapi2
-	var paths = openapi2.paths.path
-
-	console.error('paths: %s\n\n', paths)
+	const openapi2 = request.openapi2
+	const paths = openapi2.paths.path
 
 	for (var i in paths) {
 		const path = paths[i]
@@ -24,21 +21,20 @@ getStdin.buffer().then(buffer => {
 		console.error('get %s\n\n', JSON.stringify(getOperation))
 	}
 
-	var payload = {
-		errors: [],
+	const payload = {
+		errors: ["ok"],
 		files: [{
 			name: "report.txt",
 			data: Buffer.from("testing 123\n", 'utf8')
 		}]
-	};
+	}
 
 	// Verify the payload if necessary (i.e. when possibly incomplete or invalid)
-	var errMsg = Response.verify(payload);
+	const errMsg = Response.verify(payload)
 	if (errMsg)
-		throw Error(errMsg);
+		throw Error(errMsg)
 
-	var message = Response.create(payload)
-	var buffer = Response.encode(message).finish();
-	process.stdout.write(buffer);
+	const message = Response.create(payload)
+	process.stdout.write(Response.encode(message).finish())
 
-}).catch(err => console.error(err));
+}).catch(err => console.error(err))
