c454750a5657b9ccf8faad8ca04fefebfdc7fd3a
Tim Burks <timburks@google.com>
2018-02-16 22:23:27 -0800

    Simplify Swift linter, add README

--- linters/swift/gnostic-lint-responses-swift/Sources/gnostic-lint-responses-swift/main.swift
+++ linters/swift/gnostic-lint-responses-swift/Sources/gnostic-lint-responses-swift/main.swift
@@ -15,6 +15,20 @@
 import Foundation
 import Gnostic
 
+extension Gnostic_Plugin_V1_Response {
+  mutating func message(level:Gnostic_Plugin_V1_Message.Level,
+                        code:String,
+                        text:String,
+                        path:[String]=[]) {
+    var message = Gnostic_Plugin_V1_Message()
+    message.level = level
+    message.code = code
+    message.text = text
+    message.keys = path
+    messages.append(message)
+  }
+}
+
 class ResponseLinter {
   var document : Openapi_V2_Document = Openapi_V2_Document()
 
@@ -25,42 +39,40 @@ class ResponseLinter {
         let document = try Openapi_V2_Document(serializedData: model.value)
         self.document = document
         for pair in document.paths.path {
+          let path = ["paths", pair.name]
           let v = pair.value
           if v.hasGet {
-            checkOperation(v.get,
-                           path:[pair.name, "get"],
-                           response:&response)
+            checkOperation(v.get, path:path + ["get"], response:&response)
           }
           if v.hasPost {
-            checkOperation(v.post,
-                           path:[pair.name, "post"],
-                           response:&response)
+            checkOperation(v.post, path:path + ["post"], response:&response)
           }
           if v.hasPut {
-            checkOperation(v.put,
-                           path:[pair.name, "put"],
-                           response:&response)
+            checkOperation(v.put, path:path + ["put"], response:&response)
           }
           if v.hasDelete {
-            checkOperation(v.delete,
-                           path:[pair.name, "delete"],
-                           response:&response)
+            checkOperation(v.delete, path:path + ["delete"], response:&response)
           }
         }
       }
     }
   }
 
-  func resolveReference(_ reference:String) -> Openapi_V2_Schema? {
-    if reference.hasPrefix("#/definitions/") {
-      let schemaName = reference.dropFirst("#/definitions/".count)
-      for pair in document.definitions.additionalProperties {
-        if pair.name == schemaName {
-          return pair.value
+  func checkOperation(_ operation:Openapi_V2_Operation,
+                      path:[String],
+                      response:inout Gnostic_Plugin_V1_Response) {
+    for responseCode in operation.responses.responseCode {
+      let code = responseCode.name
+      if responseCode.value.response.hasSchema {
+        var schema = responseCode.value.response.schema.schema
+        if schema.ref != "" {
+          if let resolvedSchema = resolveReference(schema.ref) {
+            schema = resolvedSchema
+          }
         }
+        checkSchemaType(schema, path: path + ["responses", code, "schema"], response: &response)
       }
     }
-    return nil
   }
 
   func checkSchemaType(_ schema:Openapi_V2_Schema,
@@ -79,40 +91,17 @@ class ResponseLinter {
     }
   }
 
-  func checkOperation(_ operation:Openapi_V2_Operation,
-                      path:[String],
-                      response:inout Gnostic_Plugin_V1_Response) {
-
-    for responseCode in operation.responses.responseCode {
-      let code = responseCode.name
-      if responseCode.value.response.hasSchema {
-        let schema = responseCode.value.response.schema.schema
-        checkSchemaType(schema,
-                        path: path + ["responses", code, "schema"],
-                        response: &response)
-        if schema.ref != "" {
-          if let schema = resolveReference(schema.ref) {
-            checkSchemaType(schema,
-                            path: path + ["responses", code, "schema"],
-                            response: &response)
-          }
+  func resolveReference(_ reference:String) -> Openapi_V2_Schema? {
+    let prefix = "#/definitions/"
+    if reference.hasPrefix(prefix) {
+      let schemaName = reference.dropFirst(prefix.count)
+      for pair in document.definitions.additionalProperties {
+        if pair.name == schemaName {
+          return pair.value
         }
       }
     }
-  }
-}
-
-extension Gnostic_Plugin_V1_Response {
-  mutating func message(level:Gnostic_Plugin_V1_Message.Level,
-                        code:String,
-                        text:String,
-                        path:[String]=[]) {
-    var message = Gnostic_Plugin_V1_Message()
-    message.level = level
-    message.code = code
-    message.text = text
-    message.keys = path
-    messages.append(message)
+    return nil
   }
 }
 
@@ -120,7 +109,6 @@ func main() throws {
   let request = try Gnostic_Plugin_V1_Request(serializedData: Stdin.readall())
   var response = Gnostic_Plugin_V1_Response()
   try ResponseLinter().run(request, &response)
-  response.message(level:.info, code:"HELLO", text:"Hello from the Swift plugin!")
   let serializedResponse = try response.serializedData()
   Stdout.write(bytes: serializedResponse)
 }
