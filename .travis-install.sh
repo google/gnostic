e94491cfe876fd482981fd59b0e743b88ec2a404
Noah Dietz <ndietz@google.com>
2018-10-19 09:23:39 -0700

    update swift distro for travis

--- .travis-install.sh
+++ .travis-install.sh
@@ -14,7 +14,7 @@ cd
 mkdir -p local
 
 # Install swift
-SWIFT_URL=https://swift.org/builds/swift-4.0-branch/ubuntu1404/swift-4.0-DEVELOPMENT-SNAPSHOT-2017-09-01-a/swift-4.0-DEVELOPMENT-SNAPSHOT-2017-09-01-a-ubuntu14.04.tar.gz
+SWIFT_URL=https://swift.org/builds/swift-4.2-release/ubuntu1404/swift-4.2-RELEASE/swift-4.2-RELEASE-ubuntu14.04.tar.gz
 echo $SWIFT_URL
 curl -fSsL $SWIFT_URL -o swift.tar.gz 
 tar -xzf swift.tar.gz --strip-components=2 --directory=local
