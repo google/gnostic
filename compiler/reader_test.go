57c18005564c3af21a9325ae1cc3a667e550547a
Tim Burks <timburks@google.com>
2020-01-30 14:31:59 -0800

    Update go.mod dependency on check.v1, make references explicit.

--- compiler/reader_test.go
+++ compiler/reader_test.go
@@ -15,7 +15,7 @@
 package compiler
 
 import (
-	. "gopkg.in/check.v1"
+	"gopkg.in/check.v1"
 	"io"
 	"net/http"
 	"testing"
@@ -23,19 +23,19 @@ import (
 
 // Hook up gocheck into the "go test" runner.
 func Test(t *testing.T) {
-	TestingT(t)
+	check.TestingT(t)
 }
 
 var mockServer *http.Server
 
 type ReaderTestingSuite struct{}
 
-var _ = Suite(&ReaderTestingSuite{})
+var _ = check.Suite(&ReaderTestingSuite{})
 
-func (s *ReaderTestingSuite) SetUpSuite(c *C) {
+func (s *ReaderTestingSuite) SetUpSuite(c *check.C) {
 	mockServer = &http.Server{Addr: "127.0.0.1:8080", Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		yamlBytes, err := ReadBytesForFile("testdata/petstore.yaml")
-		c.Assert(err, IsNil)
+		c.Assert(err, check.IsNil)
 		io.WriteString(w, string(yamlBytes))
 
 	})}
@@ -44,52 +44,52 @@ func (s *ReaderTestingSuite) SetUpSuite(c *C) {
 	}()
 }
 
-func (s *ReaderTestingSuite) TearDownSuite(c *C) {
+func (s *ReaderTestingSuite) TearDownSuite(c *check.C) {
 	mockServer.Close()
 }
 
-func (s *ReaderTestingSuite) TestRemoveFromInfoCache(c *C) {
+func (s *ReaderTestingSuite) TestRemoveFromInfoCache(c *check.C) {
 	fileName := "testdata/petstore.yaml"
 	yamlBytes, err := ReadBytesForFile(fileName)
-	c.Assert(err, IsNil)
-	c.Assert(len(yamlBytes) > 0, Equals, true)
+	c.Assert(err, check.IsNil)
+	c.Assert(len(yamlBytes) > 0, check.Equals, true)
 	petstore, err := ReadInfoFromBytes(fileName, yamlBytes)
-	c.Assert(err, IsNil)
-	c.Assert(petstore, NotNil)
-	c.Assert(len(infoCache), Equals, 1)
+	c.Assert(err, check.IsNil)
+	c.Assert(petstore, check.NotNil)
+	c.Assert(len(infoCache), check.Equals, 1)
 	RemoveFromInfoCache(fileName)
-	c.Assert(len(infoCache), Equals, 0)
+	c.Assert(len(infoCache), check.Equals, 0)
 }
 
-func (s *ReaderTestingSuite) TestDisableInfoCache(c *C) {
+func (s *ReaderTestingSuite) TestDisableInfoCache(c *check.C) {
 	fileName := "testdata/petstore.yaml"
 	yamlBytes, err := ReadBytesForFile(fileName)
-	c.Assert(err, IsNil)
-	c.Assert(len(yamlBytes) > 0, Equals, true)
+	c.Assert(err, check.IsNil)
+	c.Assert(len(yamlBytes) > 0, check.Equals, true)
 	DisableInfoCache()
 	petstore, err := ReadInfoFromBytes(fileName, yamlBytes)
-	c.Assert(err, IsNil)
-	c.Assert(petstore, NotNil)
-	c.Assert(len(infoCache), Equals, 0)
+	c.Assert(err, check.IsNil)
+	c.Assert(petstore, check.NotNil)
+	c.Assert(len(infoCache), check.Equals, 0)
 	EnableInfoCache()
 }
 
-func (s *ReaderTestingSuite) TestRemoveFromFileCache(c *C) {
+func (s *ReaderTestingSuite) TestRemoveFromFileCache(c *check.C) {
 	fileUrl := "http://127.0.0.1:8080/petstore"
 	yamlBytes, err := FetchFile(fileUrl)
-	c.Assert(err, IsNil)
-	c.Assert(len(yamlBytes) > 0, Equals, true)
-	c.Assert(len(fileCache), Equals, 1)
+	c.Assert(err, check.IsNil)
+	c.Assert(len(yamlBytes) > 0, check.Equals, true)
+	c.Assert(len(fileCache), check.Equals, 1)
 	RemoveFromFileCache(fileUrl)
-	c.Assert(len(fileCache), Equals, 0)
+	c.Assert(len(fileCache), check.Equals, 0)
 }
 
-func (s *ReaderTestingSuite) TestDisableFileCache(c *C) {
+func (s *ReaderTestingSuite) TestDisableFileCache(c *check.C) {
 	DisableFileCache()
 	fileUrl := "http://127.0.0.1:8080/petstore"
 	yamlBytes, err := FetchFile(fileUrl)
-	c.Assert(err, IsNil)
-	c.Assert(len(yamlBytes) > 0, Equals, true)
-	c.Assert(len(fileCache), Equals, 0)
+	c.Assert(err, check.IsNil)
+	c.Assert(len(yamlBytes) > 0, check.Equals, true)
+	c.Assert(len(fileCache), check.Equals, 0)
 	EnableFileCache()
 }
