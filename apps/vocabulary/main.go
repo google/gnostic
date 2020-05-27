1e956c7faac7942538bff9bf0b3087e66f78cb6d
Nicoel Gizzo <gizzon@rpi.edu>
2020-05-27 20:23:05 +0000

    Added Tim's suggestions

--- apps/vocabulary/main.go
+++ apps/vocabulary/main.go
@@ -18,12 +18,9 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
-	"sort"
-	"strings"
 
 	"github.com/golang/protobuf/proto"
 
@@ -51,113 +48,37 @@ func addToMap(word string, mapName map[string]int) {
 	}
 }
 
-func createCSV(names map[string]int) {
-	f, ferror := os.Create("IndexFreqSort.csv")
-	if ferror != nil {
-		fmt.Println(ferror)
-		f.Close()
-		return
-	}
-	//Sort by value
-	type kv struct {
-		Key   string
-		Value int
-	}
-
-	var ss []kv
-	for k, v := range names {
-		ss = append(ss, kv{k, v})
-	}
-
-	sort.Slice(ss, func(i, j int) bool {
-		return ss[i].Value > ss[j].Value
-	})
-	for _, kv := range ss {
-		temp := fmt.Sprintf("%s,%d\n", kv.Key, kv.Value)
-		f.WriteString(temp)
-
-	}
-	f.Close()
-
-	//Sort alphabetically
-	key_names := make([]string, 0, len(names))
-	key_names_lower := make([]string, 0, len(names))
-	for key := range names {
-		key_names = append(key_names, key)
-		key_names_lower = append(key_names_lower, strings.ToLower(key))
-	}
-	sort.Strings(key_names)
-	sort.Strings(key_names_lower)
-	f2, ferror := os.Create("IndexAlphaSort.csv")
-	if ferror != nil {
-		fmt.Println(ferror)
-		f2.Close()
-		return
-	}
-	for _, key := range key_names {
-		temp := fmt.Sprintf("%s,%d\n", key, names[key])
-		f2.WriteString(temp)
-
-	}
-	f2.Close()
-
-	f3, ferror := os.Create("IndexAlphaLowerSort.csv")
-	if ferror != nil {
-		fmt.Println(ferror)
-		f3.Close()
-		return
-	}
-	for _, key := range key_names_lower {
-		temp := fmt.Sprintf("%s,%d\n", key, names[key])
-		f3.WriteString(temp)
-
-	}
-	f3.Close()
-
-	f4, ferror := os.Create("IndexRegular.csv")
-	if ferror != nil {
-		fmt.Println(ferror)
-		f4.Close()
-		return
-	}
-	for k, _ := range names {
-		temp := fmt.Sprintf("%s\n", k)
-		f4.WriteString(temp)
-	}
-	f4.Close()
-}
-
-func printDocument(document *pb.Document, schemas map[string]int, operationId map[string]int, names map[string]int, properties map[string]int) {
+func processDocument(document *pb.Document, schemas map[string]int, operationId map[string]int, names map[string]int, properties map[string]int) {
 	//Start
 	if document.Definitions != nil && document.Definitions.AdditionalProperties != nil {
 		for _, pair := range document.Definitions.AdditionalProperties {
 			addToMap(pair.Name, schemas)
-			printSchema(pair.Value, properties)
+			processSchema(pair.Value, properties)
 		}
 	}
 	for _, pair := range document.Paths.Path {
 		v := pair.Value
 		if v.Get != nil {
-			printOperation(v.Get, operationId, names)
+			processOperation(v.Get, operationId, names)
 		}
 		if v.Post != nil {
-			printOperation(v.Post, operationId, names)
+			processOperation(v.Post, operationId, names)
 		}
 		if v.Put != nil {
-			printOperation(v.Put, operationId, names)
+			processOperation(v.Put, operationId, names)
 		}
 		if v.Patch != nil {
-			printOperation(v.Patch, operationId, names)
+			processOperation(v.Patch, operationId, names)
 		}
 		if v.Delete != nil {
-			printOperation(v.Delete, operationId, names)
+			processOperation(v.Delete, operationId, names)
 		}
 	}
 }
 
 //^^^ Get rid of print post/get/indent
 
-func printOperation(operation *pb.Operation, operationId map[string]int, names map[string]int) {
+func processOperation(operation *pb.Operation, operationId map[string]int, names map[string]int) {
 	if operation.OperationId != "" {
 		addToMap(operation.OperationId, operationId)
 	}
@@ -183,7 +104,7 @@ func printOperation(operation *pb.Operation, operationId map[string]int, names m
 	}
 }
 
-func printSchema(schema *pb.Schema, properties map[string]int) {
+func processSchema(schema *pb.Schema, properties map[string]int) {
 	if schema.Properties != nil {
 		for _, pair := range schema.Properties.AdditionalProperties {
 			addToMap(pair.Name, properties)
@@ -214,7 +135,7 @@ func main() {
 	var properties map[string]int
 	properties = make(map[string]int)
 
-	printDocument(document, schemas, operationId, names, properties)
+	processDocument(document, schemas, operationId, names, properties)
 
 	vocab := &Vocabulary{}
 	for k, v := range schemas {
--- go.mod
+++ go.mod
@@ -4,9 +4,9 @@ go 1.12
 
 require (
 	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
-	github.com/golang/protobuf v1.4.0
+	github.com/golang/protobuf v1.4.2
 	github.com/kr/pretty v0.2.0 // indirect
-	google.golang.org/protobuf v1.21.0
+	google.golang.org/protobuf v1.23.0
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15
 	gopkg.in/yaml.v2 v2.2.2
 )
