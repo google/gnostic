1cca2a0159f81255dea2b0708d2cba972a3cc927
Koichi Shiraishi <zchee.io@gmail.com>
2021-09-26 18:18:19 +0900

    generate-gnostic: support goimports local prefix
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- generate-gnostic/main.go
+++ generate-gnostic/main.go
@@ -23,10 +23,10 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
 	"path"
-	"runtime"
 	"strings"
+
+	"golang.org/x/tools/imports"
 
 	"github.com/google/gnostic/jsonschema"
 )
@@ -199,13 +199,21 @@ func generateOpenAPIModel(version string) error {
 	log.Printf("Generating compiler support code")
 	compiler := cc.GenerateCompiler(goPackageName, License, packageImports)
 	goFileName := projectRoot + directoryName + "/" + filename + ".go"
-	err = ioutil.WriteFile(goFileName, []byte(compiler), 0644)
+
+	// format the compiler
+	log.Printf("Formatting compiler support code")
+	imports.LocalPrefix = "github.com/google/gnostic"
+	data, err := imports.Process(goFileName, []byte(compiler), &imports.Options{
+		TabWidth:  8,
+		TabIndent: true,
+		Comments:  true,
+		Fragment:  true,
+	})
 	if err != nil {
 		return err
 	}
-	// format the compiler
-	log.Printf("Formatting compiler support code")
-	return exec.Command(runtime.GOROOT()+"/bin/gofmt", "-w", goFileName).Run()
+
+	return ioutil.WriteFile(goFileName, []byte(data), 0644)
 }
 
 func usage() string {
