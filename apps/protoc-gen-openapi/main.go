5a31161272c3ce1ea4cad06091abdcdaea1c0dca
Dan Larsen <dan@techba.se>
2021-12-29 22:12:27 +0100

    Protoc gen openapi (#276)
    * Handle nested messages
    
    This incorporates @timburks suggestion for handling nested messages and their naming
    
    * Ignore services without http annotations
    
    * Move duplicated name generation code to a function
    
    * Rename test: Ignore annotaions -> Ignore services without annotaions
    
    Just to be clear what it does
    
    * Enable optional keyword
    
    It doesn't change how messages are handled
    
    * Fix query parameters
    
    This fixes issues described in #275.
    Specifically:
     - types (everything was set to string)
     - message and sub message handling
    
    Changes was done according to the note in annotations.proto:
    
    Note that fields which are mapped to URL query parameters must have a primitive type or a repeated primitive type or a non-repeated message type.
    In the case of a repeated type, the parameter can be repeated in the URL as ...?param=A&param=B.
    In the case of a message type, each field of the message is mapped to a separate parameter, such as ...?foo.a=A&foo.b=B&foo.c=C.
    
    The ability to handle sub sub messages was tested with Envoy first.
    
    * Upgrade the protoc version to be used in automated tests
    
    Only updating 2 minor versions, to keep the test using as old a protoc as possible.
    3.15.0 enables optional fields for proto3.
    
    * Enable parsing of google.api.default_host option
    
    * Add explicit field mask support to protoc-gen-openapi
    
    * fix spelling error
    
    Co-authored-by: Tim Burks <timburks@google.com>

--- apps/protoc-gen-openapi/main.go
+++ apps/protoc-gen-openapi/main.go
@@ -20,16 +20,18 @@ import (
 
 	"github.com/google/gnostic/apps/protoc-gen-openapi/generator"
 	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/pluginpb"
 )
 
 var flags flag.FlagSet
 
 func main() {
 	conf := generator.Configuration{
-		Version:     flags.String("version", "0.0.1", "version number text, e.g. 1.2.3"),
-		Title:       flags.String("title", "", "name of the API"),
-		Description: flags.String("description", "", "description of the API"),
-		Naming:      flags.String("naming", "json", `naming convention. Use "proto" for passing names directly from the proto files`),
+		Version:       flags.String("version", "0.0.1", "version number text, e.g. 1.2.3"),
+		Title:         flags.String("title", "", "name of the API"),
+		Description:   flags.String("description", "", "description of the API"),
+		Naming:        flags.String("naming", "json", `naming convention. Use "proto" for passing names directly from the proto files`),
+		CircularDepth: flags.Int("depth", 2, `depth of recursion for circular messages`),
 	}
 
 	opts := protogen.Options{
@@ -37,6 +39,9 @@ func main() {
 	}
 
 	opts.Run(func(plugin *protogen.Plugin) error {
+		// Enable "optional" keyword in front of type (e.g. optional string labe = 1;)
+		plugin.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+
 		return generator.NewOpenAPIv3Generator(plugin, conf).Run()
 	})
 }
